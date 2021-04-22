c34054d2cc4cf58c68b47f7beb61eedc282486a5
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-04-22 18:57:43 -0300

    Criado programa7-maiorde3.js

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/programa7-maiorde3.js
@@ -0,0 +1,13 @@
+let a = 50;
+let b = 50;
+let c = 50;
+
+if (a > b && a > c){
+    console.log("'A' é o maior");
+}
+else if (b > a && b > c){
+    console.log("'B' é o maior");
+}
+else {
+    console.log("'C' é o maior");
+}
