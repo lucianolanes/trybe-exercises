5c89ab6c0ddc3a272d9ffcab672e2697e2617cc9
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-05-19 20:11:41 -0300

    Resolvido exercicio 2 da parte 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/parte2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/parte2.js
@@ -11,3 +11,22 @@ const fatorial = (number) => {
 
 console.log(fatorial(7));
 
+// bônus: operador ternário
+const fatorial = number => number > 1 ? number * fatorial(number - 1) : 1
+console.log(fatorial(4));
+
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+const maiorPalavra = (frase) => {
+  const arrayPalavras = frase.split (' ');
+  let contagem = arrayPalavras[0];
+
+  for (let index = 1; index < arrayPalavras.length; index += 1){
+    if (arrayPalavras[index].length > contagem.length){
+      contagem = arrayPalavras[index];
+    }
+  }
+  return contagem;
+}
+
+console.log(maiorPalavra('Antônio foi no banheiro e não sabemos o que aconteceu'));
