cf234efb3708eb996f63f55a8919564f26dddc6f
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-05-20 19:27:06 -0300

    Resolvido exercicio 2 ate o requisito 6

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercicio2.js
@@ -0,0 +1,37 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const adicionarPropriedade = (lesson, key, value) => lesson[key] = value;
+
+const listarKeys = (lesson) => Object.keys(lesson);
+
+const tamanhoObjeto = (lesson) => Object.keys(lesson).length;
+
+const listarValues = (lesson) => Object.values(lesson);
+
+const allLessons = Object.assign({},{lesson1, lesson2, lesson3});
+
+const totalEstudantes = () => {
+  const estudantesLesson1 = allLessons.lesson1.numeroEstudantes;
+  const estudantesLesson2 = allLessons.lesson2.numeroEstudantes;
+  const estudantesLesson3 = allLessons.lesson3.numeroEstudantes;
+
+  return estudantesLesson1 + estudantesLesson2 + estudantesLesson3;
+}
