79e87fa24eb30c24ffa23f72d3b8ad4435d16cdb
Luciano Lanes <llaneslopes@gmail.com>
2021-10-24 19:43:17 -0300

    FIX

--- exercises/back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/imc.js
+++ exercises/back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/imc.js
@@ -2,10 +2,29 @@ const readline = require('readline-sync');
 
 const main = () => {
   const peso = readline.questionInt('Qual o seu peso? ');
-  const altura = readline.questionFloat('Qual a sua altura? ');
+  const altura = readline.questionFloat('Qual a sua altura? ', 2);
+  const result = peso / (altura * altura);
+  if (result < 18.5) {
+    return console.log('Abaixo do Peso (magreza)');
+  }
+  
+  if (result >= 18.5 && result < 25) {
+    return console.log('Peso normal');
+  }
+  
+  if (result >= 25 && result < 30) {
+    return console.log('Acima do peso (sobrepeso)');
+  }
 
-  const result = (altura * peso) / 2;
-  return console.log(result);
+  if (result >= 30 && result < 35) {
+    return console.log('Obesidade grau I');
+  }
+
+  if (result >= 35 && result < 40) {
+    return console.log('Obesidade grau II');
+  }
+
+    return console.log('Obesidade grau III e IV');
 }
 
 main();
